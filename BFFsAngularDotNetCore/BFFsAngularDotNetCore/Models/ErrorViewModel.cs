1727d94bf1da97abdb1bbe792fdd3936a6fc0d6a
Wasim Hanna <wfhanna1@gmail.com>
2018-04-12 22:48:03 -0400

    Basic working solution

---
+++ BFFsAngularDotNetCore/BFFsAngularDotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BFFsAngularDotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
